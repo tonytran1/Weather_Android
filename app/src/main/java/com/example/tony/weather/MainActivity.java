33d75800003e9016226fbd5231b0b5524a224cc1
tonytran1 <tonylyfe@gmail.com>
2016-03-10 01:02:00 -0800

    Integrated changing cities

--- app/src/main/java/com/example/tony/weather/MainActivity.java
+++ app/src/main/java/com/example/tony/weather/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.tony.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  *  Author: Tony Tran
@@ -19,8 +21,24 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        // Disable browser from opening
+        myWebView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        myWebView.setWebChromeClient(new WebChromeClient());
         myWebView.loadUrl("http://tonytran1.github.io/Weather/");
+
+        // Enable Javascript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
     }
 }
